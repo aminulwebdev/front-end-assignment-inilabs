2b1607715d949ac13b165d8fef3461f02056e074
AminulAnik <imaminul012@gmail.com>
2024-03-03 12:08:40 +0600

    Collaborate part added

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ tailwind.config = {
       fontFamily: {
         'poppins': "'Poppins', sans-serif",
         'inter': "'Inter', sans-serif",
-        'sanss': 'Helvetica, Arial, sans-serif',
+        'helt': 'Helvetica, Arial, sans-serif',
       },
       backgroundImage: {
         'banner': " linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('./images/banner.jpg')",
