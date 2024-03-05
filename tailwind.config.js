29df7c00faf8783bff6f79aa06869dca03632187
AminulAnik <imaminul012@gmail.com>
2024-03-05 09:21:10 +0600

    Tailwind CDN to CLI Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
-tailwind.config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
       colors: {
         primary: "#050038",
         secondary: "#4262FF",
-       
+
       },
       maxWidth: {
         container: "1140px",
@@ -14,14 +16,18 @@ tailwind.config = {
         'inter': "'Inter', sans-serif",
         'helt': 'Helvetica, Arial, sans-serif',
       },
-      screens: {
-        'xs': '350px',
-        'sm': '576px',
-        'md': '768px',
-        'lg': '992px',
-        'xl': '1200px',
-        'xxl': '1400px',
-      },
-    }
-  }
+    },
+    screens: {
+      'xs': '350px',
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
+      'xxl': '1400px',
+    },
+  },
+  plugins: [],
 }
+
+
+
